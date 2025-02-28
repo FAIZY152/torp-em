93cd50e27fa0bb59bdaa1ef843596f710012e2f3
faizy15246 <faizybangash05@gmail.com>
2025-02-28 14:43:36 +0500

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,22 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-}
+  title: "Protfolio",
+  description: "Thats me in a portfolio",
+  icons: {
+    icon: "/profile.jpg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
