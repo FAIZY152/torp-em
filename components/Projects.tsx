d45ed371ef259df35c04b1549501cbd9a76a528c
Muhammad Fayaz <fayazjani7576@gmail.com>
2025-04-11 09:32:42 +0500

    add-new

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -36,7 +36,8 @@ const projects = [
     githubLink: "https://github.com/FAIZY152/my-app-1",
   },
   {
-    image: "https://ideausher.com/wp-content/uploads/2023/05/Integrating-AI-with-Existing-SaaS-1024x576.webp",
+    image:
+      "https://ideausher.com/wp-content/uploads/2023/05/Integrating-AI-with-Existing-SaaS-1024x576.webp",
     title: "Ai Saas App",
     description:
       "An AI-powered SaaS app Using open-source APIs like Google Gemini, Pollination AI, and background remover APIs. It includes an AI chatbot, image generation, code generation, automated code review, and background removal for enhanced productivity and creativity.",
@@ -47,6 +48,7 @@ const projects = [
       "Prisma",
       "Zustand",
       "OpenAI APIs",
+      "Gemini APIs",
     ],
     features: [
       "Real-time Ai Assistant",
