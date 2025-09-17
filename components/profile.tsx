d59c076d44135d042c3db6344c14941b1b1e855e
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2025-09-17 12:49:57 +0500

    Update profile.tsx

--- components/profile.tsx
+++ components/profile.tsx
@@ -75,15 +75,6 @@ I’m always eager to learn, build, and solve problems using clean and efficient
             Download CV
           </Button>
 
-          <a 
-  href="mailto:developer15246@gmail.com" 
-  target="_blank" 
-  rel="noopener noreferrer"
->
-  <Button variant="outline" className="text-gray-800 mt-1 md:mt-0">
-    Email me
-  </Button>
-</a>
         </div>
       </motion.div>
       <motion.div
